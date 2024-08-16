00d75eeed95c5b5e6e626fc81792b279a191c7e6
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-08-16 17:58:25 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -eux
 GCC_VERSION=10.5.0
 MINGW_VERSION=6.0.1
 WXMSW_VERSION=3.2.5
-BUILD_NO=20-stl-cb-clang-mingw32
+BUILD_NO=21-stl-cb-winlibs1420
 HOME=$(cygpath -m /home)
 NAME=wxWidgets-${WXMSW_VERSION}
 
@@ -16,8 +16,10 @@ cd /home
 #wget -q https://github.com/brechtsanders/winlibs_mingw/releases/download/14.1.0posix-18.1.7-12.0.0-ucrt-r2/winlibs-i686-posix-dwarf-gcc-14.1.0-mingw-w64ucrt-12.0.0-r2.7z
 # 7z x gcc-v${GCC_VERSION}-mingw-w64-v${MINGW_VERSION}-i686.7z -r -o/home
 #7z x winlibs-i686-posix-dwarf-gcc-14.1.0-mingw-w64ucrt-12.0.0-r2.7z -r -o/home
+wget -q https://github.com/brechtsanders/winlibs_mingw/releases/download/14.2.0posix-18.1.8-12.0.0-ucrt-r1/winlibs-i686-posix-dwarf-gcc-14.2.0-mingw-w64ucrt-12.0.0-r1.7z
+7z x winlibs-i686-posix-dwarf-gcc-14.2.0-mingw-w64ucrt-12.0.0-r1.7z -o/home
 
-#export PATH=/home/mingw32/bin/:$PATH
+export PATH=/home/mingw32/bin/:$PATH
 
 wget -q https://github.com/wxWidgets/wxWidgets/releases/download/v${WXMSW_VERSION}/wxWidgets-${WXMSW_VERSION}.tar.bz2
 tar -jxf ./wxWidgets-${WXMSW_VERSION}.tar.bz2
@@ -28,7 +30,7 @@ cp -f ./setup.h ./wxWidgets-${WXMSW_VERSION}/include/wx/msw/
 cp -f ./config.gcc ./wxWidgets-${WXMSW_VERSION}/build/msw/
 cd wxWidgets-${WXMSW_VERSION}/build/msw
 mingw32-make -f makefile.gcc setup_h
-mingw32-make -f makefile.gcc -j16
+mingw32-make -f makefile.gcc -j$(nproc)
 
 cp /home/setup.h /home/wxWidgets-${WXMSW_VERSION}/lib/
 
